a6bdfa2f84f50977e4d4d993b212b141bf6f8ebd
Rachel Bitting <bittingr@flip3.engr.oregonstate.edu>
2018-10-28 21:27:38 -0700

    Adding testme.c quiz

---
+++ projects/bittingr/quiz/testme.c
@@ -0,0 +1,82 @@
+/**************************
+
+Rachel Bitting
+October 28, 2018
+CS362 - testme.c Quiz
+
+/**************************/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+	// Initialize array of chars
+	char charArr[9] = {'[','(','{',' ','a','x','}',')',']'};
+	
+	// Get random char from array
+	int random = rand() % 9;
+	
+    return charArr[random];
+}
+
+char *inputString()
+{	
+	// Initialize array of chars
+	char charArr[9] = {'r','e','s','e','t','\0'};
+	
+	// Initialize string
+	char* string = (char *) malloc(6);
+	
+	// Loop through string and assign chars
+	int i;
+	for (i=0;i<6;i++) {
+		// Set char at position i to random char in char array
+		string[i] = charArr[rand() % 6];
+	}
+	
+    return string;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1; 	// 91
+    if (c == '(' && state == 1) state = 2; 	// 40
+    if (c == '{' && state == 2) state = 3; 	// 123
+    if (c == ' '&& state == 3) state = 4; 	// 32
+    if (c == 'a' && state == 4) state = 5;	// 97
+    if (c == 'x' && state == 5) state = 6;	// 120
+    if (c == '}' && state == 6) state = 7;	// 125
+    if (c == ')' && state == 7) state = 8;	// 41
+    if (c == ']' && state == 8) state = 9;	// 93
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
