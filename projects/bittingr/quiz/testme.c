6a61d997d140737ed17d24c242f403bc7946c88d
Rachel Bitting <bittingr@flip3.engr.oregonstate.edu>
2018-10-28 21:38:18 -0700

    Last updates and added Makefile

--- projects/bittingr/quiz/testme.c
+++ projects/bittingr/quiz/testme.c
@@ -1,10 +1,10 @@
-/**************************
+/***************************
 
 Rachel Bitting
 October 28, 2018
 CS362 - testme.c Quiz
 
-/**************************/
+***************************/
 
 #include<stdio.h>
 #include<string.h>
@@ -53,15 +53,15 @@ void testme()
     s = inputString();
     printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
 
-    if (c == '[' && state == 0) state = 1; 	// 91
-    if (c == '(' && state == 1) state = 2; 	// 40
-    if (c == '{' && state == 2) state = 3; 	// 123
-    if (c == ' '&& state == 3) state = 4; 	// 32
-    if (c == 'a' && state == 4) state = 5;	// 97
-    if (c == 'x' && state == 5) state = 6;	// 120
-    if (c == '}' && state == 6) state = 7;	// 125
-    if (c == ')' && state == 7) state = 8;	// 41
-    if (c == ']' && state == 8) state = 9;	// 93
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
     if (s[0] == 'r' && s[1] == 'e'
        && s[2] == 's' && s[3] == 'e'
        && s[4] == 't' && s[5] == '\0'
